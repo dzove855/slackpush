b58cfd83cb67a0cad3009eb0501a768cfcfe2e61
Dzogovic Vehbo <vehbo97@gmail.com>
2017-09-26 00:53:44 +0200

    Create Config.pm

---
+++ lib/Slackpush/Config/Config.pm
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+
+package Slackpush::Config::Config;
+use strict;
+use warnings;
+use Config::INI::Writer;
+use Config::INI::Reader;
+use vars qw($VERSION @ISA @EXPORT @EXPORT_OK %EXPORT_TAGS);
+
+$VERSION     = 1.00;
+@ISA         = qw(Exporter);
+@EXPORT      = ();
+@EXPORT_OK   = qw(new);
+%EXPORT_TAGS = ( DEFAULT => [qw(&new)]);
+
+sub new{
+    my $config = shift;
+    my $self = {
+    };
+
+    bless $self, $config;
+    return $self;    
+};
+
+sub setOpt{
+    my ($self, $obj, $value) = @_;
+    $self->{$obj} = $value if defined($obj) && defined($value);
+    return($self->{$obj});
+};
+
+sub save{
+    my ($self, $configfile) = @_;
+
+    return "Error" if ! defined($configfile);
+
+    foreach my $key (keys %{$self}) {
+       Config::INI::Writer->write_file({'_' => { $key => $self->{$key}}}, $configfile);
+    }
+    chmod 0600, $configfile;
+
+    return 1;
+};
+
+sub read{
+    my ($self, $configfile, $value) = @_;
+    return "Error" if ! defined($configfile) or ! defined($value);
+    my $config_hash = Config::INI::Reader->read_file($configfile);
+    return $config_hash->{_}->{$value} if defined($config_hash->{_}->{$value});
+
+};
+
+1;
+
